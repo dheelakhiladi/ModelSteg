70911753f43c453980bf8a8f1b91eeb46c859e52
Mohit <mohitaquarian97@gmail.com>
2017-09-21 02:29:08 +0530

    ->Binary text from file.

--- Project.py
+++ Project.py
@@ -1,9 +1,16 @@
-import cv2.cv as cv
-image=cv.LoadImage('/home/hitman/Desktop/ocv.jpg',cv.CV_LOAD_IMAGE_UNCHANGED)
-font= cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 4, 4, 0, 10, 0)
-x=image.height/4
-y=image.width
-cv.PutText(image,"Mohit 20 M",(x,y),font,cv.RGB(255,255,255))
-cv.NamedWindow('MohitTheGunda',400*200)
-cv.ShowImage('MohitTheGunda',image)
-cv.WaitKey(0)
+import binascii as ba
+import cv2 as cv
+import numpy as np 
+from matplotlib import pyplot as plt
+image=cv.imread('/home/hitman/Documents/Study/Sem7/PBL(s)/InformationSecurity/Lenna.png',cv.CV_LOAD_IMAGE_GRAYSCALE)
+with open ("/home/hitman/Documents/WORK/Development/Development/ISP/data.txt","r") as file:
+	label=file.read()
+	label=label[:-1]
+bintext=bin(int(ba.hexlify(label),16))
+bintext=bintext[2:]
+for i in range(image.shape[0]):
+	for j in range (image.shape[1]):
+		print bin(image[i][j])[2:]
+cv.imshow('Lena',image)
+cv.waitKey(0)
+cv.destroyAllWindows()
